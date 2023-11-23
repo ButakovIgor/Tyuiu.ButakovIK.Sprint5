958f12acff2c4160361402fb15ea3522dbb20ad3
ButakovIgor <t4w3rr@gmail.com>
2023-11-23 06:56:16 +0500

    Task6

---
+++ Tyuiu.ButakovIK.Sprint5.Task6.V26.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ButakovIK.Sprint5.Task6.V26.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ButakovIK.Sprint5.Task6.V26.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("77357b3f-7658-4f1c-9791-fc0b46b7955a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ButakovIK.Sprint5.Task6.V26/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Tyuiu.ButakovIK.Sprint5.Task6.V26.Lib;
+
+namespace Tyuiu.ButakovIK.Sprint5.Task6.V26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Бутаков И.К. | АСОиУБ-23-1";
+
+            Console.WriteLine("*****************************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                             *");
+            Console.WriteLine("* Тема: Обработка текстовых файлов                                                      *");
+            Console.WriteLine("* Таск #6                                                                               *");
+            Console.WriteLine("* Вариaнт #26                                                                           *");
+            Console.WriteLine("* Выполнил: Бутаков Игорь Константинович | АСОиУБ-23-1                                  *");
+            Console.WriteLine("*****************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                              *");
+            Console.WriteLine("* Дан файл InPutDataFileTask6V26.txt Создать папку в ручную и скопировать               *");
+            Console.WriteLine("* в неё файл в котором есть набор символьных данных. Найти количество знаков вопроса.   *");
+            Console.WriteLine("*****************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                      *");
+            Console.WriteLine("*****************************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask6V26.txt";
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("*****************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТAТ:                                                                            *");
+            Console.WriteLine("*****************************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
